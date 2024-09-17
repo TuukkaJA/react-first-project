e6d6fe714b4df9bd356f751f064342fb0b588b11
Tuukka Järviluoma <tuukka.jarviluoma@gmail.com>
2024-09-17 18:42:19 +0300

    tehtävä 6-7

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import Expenses from './components/Expenses';
+import React from 'react';
+import Expenses from './components/Expenses/Expenses';
 
 
-function App() {
+const App = () => {
 
   const expenses = [
     {
@@ -10,7 +11,10 @@ function App() {
       amount: 94.12,
       date: new Date("2022-07-14"),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date("2023-01-12") },
+    { id: 'e2', 
+     title: 'New TV', 
+     amount: 799.49, 
+     date: new Date("2022-08-12") },
     {
       id: 'e3',
       title: 'Car Insurance',
@@ -21,7 +25,7 @@ function App() {
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
-      date: new Date("2022-05-12"),
+      date: new Date("2023-01-09"),
     },
   ];
 
